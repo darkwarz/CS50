25414e175a78ed54e754748d905ee3551fce75d8
jabier espinal <jabierespinal@gmail.com>
2018-04-03 12:13:54 +0000

    Pset1 added

---
+++ Pset1/number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
+
+//int stuff()
+//{
+//  printf("stuff\n");
+//  return 1;
+// }
+
+// int stuffs(char* stuff)
+//{
+//  printf("%\n", stuff);
+//  return 1;
+//  }
+
+int main(void)
+{
+    printf("Hello there\n");
+    int integer = 5 + 8;
+// 000101 + 001000 = 001101
+    printf("Here's my number: %i\n", integer);
+    float flt = 5.3 - 4.83;
+    printf("Here's my Float: %.20f\n", flt);
+}
