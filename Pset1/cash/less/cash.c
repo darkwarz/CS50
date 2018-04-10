4f7e270a149c3d2ebb29e49fb0b8bad9df233a34
jabier espinal <jabierespinal@gmail.com>
2018-04-10 12:03:22 +0000

    updated some code

--- Pset1/cash/less/cash.c
+++ Pset1/cash/less/cash.c
@@ -5,7 +5,7 @@
 // by : Jabier Espinal
 int main(void)
 {
-    float dollars;
+    float dollars = 0;
     do
     {
         dollars = get_float("Change owed: ");
@@ -13,31 +13,42 @@ int main(void)
     while (dollars < 0);
 
     int cents = round(dollars * 100);
+
 // Quaters
     int coins = 0;
+    int quaters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennys = 0;
+
     while (cents >= 25)
     {
         cents -= 25;
         coins++;
+        quaters++;
     }
 // Dimes
     while (cents >= 10)
     {
         cents -= 10;
         coins++;
+        dimes++;
     }
 // Nickels
     while (cents >= 5)
     {
         cents -= 5;
         coins++;
+        nickels++;
     }
 // Pennys
     while (cents >= 1)
     {
         cents -= 1;
         coins++;
+        pennys++;
     }
 
-    printf("%i\n", coins);
+    printf("Change in each coin:\n - quaters %i\n - dimes %i\n - nickels %i\n - pennys %i\n", quaters, dimes, nickels, pennys);
+    printf("Here is your coins total: %i\n", coins);
 }
